91e6ef7dc23aedfc8654b1f70400a1953e268aeb
ShahzadMalik07 <shahzadmalik425@gmail.com>
2024-12-26 23:34:32 +0530

    project details added

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -48,7 +48,7 @@ const projects = [
     num: "04",
     title: "Next Js FullStack Project",
     category: "Fullstack",
-    description: "I developed an anonymous messaging website using Next.js, featuring email OTP verification for secure user registration. Each user receives a unique profile link, allowing others to send them anonymous messages. The platform includes a dashboard where users can toggle message acceptance, view message cards, delete messages, and leverage AI-powered suggestions for crafting messages. These features, combined with a seamless interface, offer an engaging and interactive experience for anonymous communication",
+    description: "I developed an anonymous messaging website using Next.js, featuring email OTP verification for secure user registration. Each user receives a unique profile link, allowing others to send them anonymous messages. The platform includes a dashboard where users can toggle message acceptance, view message cards, delete messages, and leverage AI-powered suggestions for crafting messages. These features, combined with a seamless interface, offer an engaging and interactive experience for anonymous communication.",
     stack: [{ name: "NextJs" }, { name: "TypeScript" }, { name: "MongoDb" }, {name: "AI"}, {name:"Shadcn/ui"},{name:"Resend/Email"}],
     image: "/Ama.png",
     live: "https://anonymous-message-fsngglnss-shahzadmalik07s-projects.vercel.app/",
@@ -57,14 +57,14 @@ const projects = [
   },
   {
     num: "05",
-    title: "Ract Js Frontend Project",
+    title: "Youtube Frontend Project",
     category: "Frontend",
-    description: "I developed an anonymous messaging website using Next.js, featuring email OTP verification for secure user registration. Each user receives a unique profile link, allowing others to send them anonymous messages. The platform includes a dashboard where users can toggle message acceptance, view message cards, delete messages, and leverage AI-powered suggestions for crafting messages. These features, combined with a seamless interface, offer an engaging and interactive experience for anonymous communication",
-    stack: [{ name: "NextJs" }, { name: "TypeScript" }, { name: "MongoDb" }, {name: "AI"}, {name:"Shadcn/ui"},{name:"Resend/Email"}],
-    image: "/Ama.png",
+    description: "Built a YouTube clone app featuring video browsing, search with debouncing, and a nested comments section. Leveraged the YouTube Suggestions API with caching for enhanced performance and user experience. Integrated a live chat feature on the video play page to enable real-time user interaction. Focused on creating a responsive and intuitive interface while efficiently handling real-time data and complex interactions.",
+    stack: [{ name: "ReactJs" }, { name: "Redux" }, {name: "Tailwind"}, {name:"Youtube API's"}],
+    image: "/yotube.png",
     live: "https://anonymous-message-fsngglnss-shahzadmalik07s-projects.vercel.app/",
-    github: "https://github.com/ShahzadMalik07/Anonymous-Message-App",
-    link: true
+    github: "https://github.com/ShahzadMalik07/Youtube",
+    link: false
   },
 
 ]
